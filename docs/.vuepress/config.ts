684ff3ca0bf06de9d0f139eca0f7d8e59d635f8c
baiyemac <1159428886@qq.com>
2022-03-29 23:49:57 +0800

    updata:心情杂货

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -90,7 +90,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
         items: [
           { text: "学习", link: "/pages/f2a556/" },
           { text: "面试", link: "/pages/aea6571b7a8bae86/" },
-          { text: "心情杂货", link: "/pages/2d615df9a36a98ed/" },
+          { text: "心情杂货", link: "/pages/567c08/" },
           { text: "实用技巧", link: "/pages/baaa02/" },
           { text: "友情链接", link: "/friends/" },
         ],
